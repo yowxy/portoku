90bf76da58f939de0a3b532304755740ea9bcd5d
yowxy <iklilblender@gmail.com>
2025-11-16 19:57:55 +0700

    fix: link Linkedin

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -6,7 +6,7 @@ const FooterPage = () => {
                 <p className="text-white mt-2">Available for Your Next Project</p>
                 <div className="mt-4">
                     <button className="bg-white  w-[264px] h-[68px] rounded-3xl font-semibold text-3xl">
-                        <a href="https://www.linkedin.com/in/iklil-najmi-hamzahhhh/" target="_blank">
+                        <a href="https://www.linkedin.com/in/iklilnajmi/" target="_blank">
                         Lets  Connect
                         </a>
                     </button>
